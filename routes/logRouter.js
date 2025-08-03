44e31228aa5217146d3c77fa57cfca4649c89d71
AJ <121802680+aj-codess@users.noreply.github.com>
2025-08-03 16:30:50 -0400

    typo fixed

--- routes/logRouter.js
+++ routes/logRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 import loginController from "./../controller/loginController.js";
 
-const xlogRouter = express.Router();
+const logRouter = express.Router();
 
 logRouter.post("/",(req,res)=>{
     loginController.login(req,res);
 });
-x
+
 export default logRouter;
